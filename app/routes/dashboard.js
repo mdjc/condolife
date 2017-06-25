b4be8203b8d45bd0a9e738f0eda0894b29831bbc
mdjc <mirnadjc@gmail.com>
2017-06-25 00:09:15 -0400

    add monthly balance to dashboard

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -10,10 +10,18 @@ export default Ember.Route.extend({
         let buildingId = params.buildingId;
         this.get('session').setCurrentBuilding(buildingId);
 
+        var dateUtils = this.get('dateUtils');
+        var today = new Date();
+        var beginningOfMonth = dateUtils.beginningOfMonth(today);
+        var endOfMonth = dateUtils.endOfMonth(today);
+        var from = dateUtils.toStr(beginningOfMonth);
+        var to = dateUtils.toStr(endOfMonth);
+
         return RSVP.hash({
            b: this.getBuilding(buildingId),
            bs: this.getBuildingStats(buildingId),
-           payments: this.getPayments(buildingId)
+           mb: this.getMonthlyBalance(buildingId, from, to),
+           payments: this.getPayments(from, to)
         });        
     },
 
@@ -29,15 +37,19 @@ export default Ember.Route.extend({
             });
     },
 
-    getPayments() {
-        var dateUtils = this.get('dateUtils');
-        var today = new Date();
-        var beginningOfMonth = dateUtils.beginningOfMonth(today);
-        var endOfMonth = dateUtils.endOfMonth(today);
+    getMonthlyBalance(buildingId, from, to) {
+         return this.get('ajax')
+            .request(`/buildings/${buildingId}/payments/stats`, {
+                crossDomain: true,
+                xhrFields: { withCredentials: true },
+                data: {'from': from, 'to': to}
+            });
+    },
 
+    getPayments(from, to) {
         return this.get('store').query('payment', 
-            {'from': dateUtils.toStr(beginningOfMonth), 
-            'to': dateUtils.toStr(endOfMonth), 
+            {'from': from, 
+            'to': to, 
             'limit':'10',
             'order' : 'desc'});
     }
