b9fe2daea8901d1f4f592866b296fe572be45d6c
mdjc <mirnadjc@gmail.com>
2017-08-19 17:49:10 -0400

    show resident's apartment name

--- app/routes/condo.js
+++ app/routes/condo.js
@@ -1,10 +1,31 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
     ajaxHelper: Ember.inject.service(),
+    session: Ember.inject.service(),
 
     model(params) {    
         let condoId = params.condoId;
+
+        return RSVP.hash({
+            condo: this.getCondo(condoId),
+            apartment: this.getApartmentIfResident()
+        });
+    },
+
+    getCondo(condoId) {
         return this.get('ajaxHelper').requestJson(`condos/${condoId}`);
+    },
+
+    getApartmentIfResident() {
+        let role = this.get('session').currentUserRole;
+
+        if (role === 'RESIDENT') {
+            let username = this.get('session').currentUsername;
+            return this.get('ajaxHelper').requestJson(`apartments/${username}`);
+        }
+
+        return "";
     }
 });
